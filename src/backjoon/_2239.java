4853bc15321aef175ac4a82788443d5f9c9fa5bc
yunhozz <qkrdbsgh1121@naver.com>
2024-10-06 11:12:44 +0900

    [G4] 수도쿠

--- src/backjoon/_2239.java
+++ src/backjoon/_2239.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Main {
+public class _2239 {
 
     static int[][] board;
     static final List<int[]> coordinates = new ArrayList<>();
